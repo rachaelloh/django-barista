d288d3e7314bcacc56870c9ff9379f33db313b41
Ubuntu <ubuntu@ip-172-31-81-125.ec2.internal>
2020-02-01 08:54:15 +0000

    created master layout template

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 # Create your views here.
+
+# Show Products
+def show_products(request):
+    all_products = Product.objects.all()
+    return render(request, 'catalog/products.template.html', {
+        'all_products':all_products
+    })
--- coffee/wsgi.py
+++ coffee/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for barista_course project.
+WSGI config for coffee project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barista_course.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee.settings')
 
 application = get_wsgi_application()
