18b01327a7047d4f46bc99385b46eed6097e281a
Ubuntu <ubuntu@ip-172-31-81-125.ec2.internal>
2020-02-02 14:58:00 +0000

    route for delete function

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import show_products, create_product, update_product, confirm_delete_product
+from .views import show_products, create_product, update_product, confirm_delete_product, actually_delete_product
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create', create_product),
     path('update/<product_id>', update_product, name='update_product'),
     path('confirm_delete/<product_id>', confirm_delete_product),
+    path('actually_delete/<product_id>', actually_delete_product, name='delete_product'),
 ]
