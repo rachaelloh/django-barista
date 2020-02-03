7935c85db93268bd14a680ed90d97e61977b6619
Ubuntu <ubuntu@ip-172-31-81-125.ec2.internal>
2020-02-03 03:15:36 +0000

    add imagefield to models and migrate.

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from .models import Product, Category
+from pyuploadcare.dj.forms import ImageField, FileWidget
 
 class ProductForm(forms.ModelForm):
+    image = ImageField(widget=FileWidget(attrs={'data-clearable':True}))
     class Meta:
         model=Product
-        fields=('name', 'price', 'stock', 'Category')
+        fields=('name', 'price', 'stock', 'Category', 'image')
--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
 class Product(models.Model):
@@ -6,6 +7,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2, blank=False)
     stock = models.IntegerField(blank=False)
     Category = models.ForeignKey("Category", blank=True, null=True, on_delete=models.SET_NULL)
+    
+    image = ImageField(blank=True)
     
     def __str__(self):
         return self.name + " $" + str(self.price) + " (x" + str(self.stock) + ")"
